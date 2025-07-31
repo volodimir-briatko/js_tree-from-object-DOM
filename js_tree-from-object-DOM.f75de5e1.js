87ecd43d4f5b270e6284b36d856f892d4154660f
volodimir-briatko <volodimirbryatko@gmail.com>
2025-07-31 23:20:36 +0300

    make build

---
+++ js_tree-from-object-DOM.f75de5e1.js
@@ -0,0 +1,33 @@
+'use strict';
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector('#tree');
+function createTree(element, data) {
+    if (typeof data === 'object' && data !== null && Object.keys(data).length > 0) {
+        const ul1 = document.createElement('ul');
+        element.append(ul1);
+        for(const key in data){
+            const li1 = document.createElement('li');
+            li1.textContent = key;
+            ul1.append(li1);
+            if (typeof data[key] === 'object' && data[key] !== null) createTree(li1, data[key]);
+        }
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=js_tree-from-object-DOM.f75de5e1.js.map
